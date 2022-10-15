fe3bcef6dae1142542a2563871691922022b18be
caocao <pgcao@qq.com>
2022-10-15 11:51:11 +0800

    初次提交

---
+++ src/App.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Iocphp;
+
+class App
+{
+    private function __construct()
+    {
+    }
+
+    /**
+     * 框架服务运行入口.
+     *
+     * @param string             $namespace 应用命名空间
+     * @param class-string<IApp> $app
+     */
+    public static function run(string $app, ?callable $callback = null): void
+    {
+        echo 'Hello world';
+    }
+}
